ee544a257dfe6fee2e5912a7a493e35c3fa6fa76
Raj Mevawala <rajmevawala23@icloud.com>
2026-09-15 15:15:05 +0530

    [IMP] Add authentication UI and configuration

--- resolve_it/rsit/Program.cs
+++ resolve_it/rsit/Program.cs
@@ -87,7 +87,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Account}/{action=Login}/{id?}")
     .WithStaticAssets();
 
 app.Run();
